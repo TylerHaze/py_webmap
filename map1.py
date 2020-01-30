01f55a06b2bce551b1e0e1b81d0d12372fe5a9e1
Tyler Hazleton <tylerhazleton@gmail.com>
2020-01-29 22:17:55 -0800

    added layer control

--- map1.py
+++ map1.py
@@ -11,7 +11,7 @@ elev = list(df["ELEV"])
 
 map = folium.Map(location=(38.85, -99.09), zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fg = folium.FeatureGroup(name="USA Volcanoes")
 
 for lt, ln, el in zip(volcanoe_lat, volcanoe_lon, elev):
     if el < 1000:
@@ -22,8 +22,11 @@ for lt, ln, el in zip(volcanoe_lat, volcanoe_lon, elev):
         color = 'green'
     fg.add_child(folium.CircleMarker(location=[lt, ln], popup=el, fill_color=color, radius=6, fill_opacity=1, color='gray'))
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+fg2 = folium.FeatureGroup(name="Population Colors")
+fg2.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read()),style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
+map.add_child(fg2)
+map.add_child(folium.LayerControl())
 map.save("Map1.html")
